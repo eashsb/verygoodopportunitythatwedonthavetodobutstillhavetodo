4afefbced6d3acc58b8a9e6358c2caf034c36ce7
eashsb <153099906+eashsb@users.noreply.github.com>
2024-07-18 12:18:08 +0000

    snazzzyzyzyzyzyz

--- snazzy.py
+++ snazzy.py
@@ -19,8 +19,11 @@ def main_menu():
         enter_airport_details()
     elif option == "F":
         enter_flight_details()
+    elif option == "P":
+        enter_price_plan_calc_profit()
 
 def enter_airport_details():
+    global airportCode,  overseasAirportCode
     airportCode = input("Enter the 3 character airport code (UK Only): ").upper()
     if airportCode != "LPL" and airportCode != "BOH":
         print("you suck")
@@ -34,6 +37,7 @@ def enter_airport_details():
                 found=True
                 print("")
                 print(f"The airport is {row[1]}.")
+                print("Thanks, returning to menu now")
                 break
         if not(found):
             print("you suck")
@@ -41,24 +45,52 @@ def enter_airport_details():
         main_menu()
 
 def enter_flight_details():
-    aircrafts = []
+    global aircraft, firstClassSeats, standardClassSeats
     print("Enter the aircraft type below")
     print("MN = medium narrow")
     print("LN = large narrow")
     print("LW = large wide")
+    print("-------------------------------------------------------")
     aircraft = input("Enter Here: ").lower()
+    print("-------------------------------------------------------")
+
     if aircraft=="mn":
         x=0
     elif aircraft=="ln":
         x=1
     elif aircraft=="lw":
         x=2
-    print("Type of aircraft:", aircrafts[x][0])
-    print("Running Cost:", aircrafts[x][1])
-    print("Max Flight Range:", aircrafts[x][2])
-    print("Capacity with all standard seats:", aircrafts[x][3])
-    print("Min First Class Seats:", aircrafts[x][4])
+    else:
+        x=3
+    if x!=3:
+        print("Type of aircraft:", aircrafts[x][0])
+        print("Running Cost:", aircrafts[x][1])
+        print("Max Flight Range:", aircrafts[x][2])
+        print("Capacity with all standard seats:", aircrafts[x][3])
+        print("Min First Class Seats:", aircrafts[x][4])
+        print("-------------------------------------------------------")
+        firstClassSeats = int(input("Enter the number of first class seats on the plane: "))
 
+        if firstClassSeats < aircrafts[x][4]:
+            print("you suck, not enough first class seats entered")
+        
+        elif firstClassSeats > aircrafts[x][3] / 2:
+            print("you suck, too many first class seats entered")
+        else:
+            standardClassSeats = aircrafts[x][3] - (firstClassSeats * 2)
+            print("Thanks, returning to menu now")
+    else:
+        print("you suck")
+    main_menu()
 
+def enter_price_plan_calc_profit():
+    if 'airportCode' and 'overseasAirportCode' in globals():
+        if 'aircraft' in globals():
+            if'firstClassSeats' in globals():
+                if airportCode == "LPL":
+                    
+    else:
+        print("Make sure to first enter you airport codes!")
+    main_menu()
 
 main_menu()
